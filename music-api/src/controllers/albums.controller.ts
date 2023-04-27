04e5aba091c2ef90f2a1d7c22e3d72e261d5b33e
nabi kamila <kamilanabi98@gmail.com>
2023-04-27 16:51:24 +0600

    #2 Написала контроллер для GET POST запросов по альбому

---
+++ music-api/src/controllers/albums.controller.ts
@@ -0,0 +1,73 @@
+import express, { Request, Response } from "express";
+import Album from "@src/models/Album";
+import { nanoid } from "nanoid";
+import multer from "multer";
+import path from "path";
+import { albumPath } from "config";
+import { CreateAlbumDto } from "@src/dto/CreateAlbum.dto";
+
+const controller = express.Router();
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, albumPath);
+  },
+  filename(req, file, cb) {
+    cb(null, nanoid() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage });
+
+controller.get("/", async (req: Request, res: Response) => {
+  try {
+    if (req.query.artist) {
+      const artist = req.query.artist as string;
+      const result = await Album.find({artist: artist});
+      if (result) {
+        res.send(result);
+      } else {
+        res.sendStatus(400);
+      }
+    } else {
+      const artists = await Album.find();
+      res.send(artists);
+    }
+  } catch (e) {
+    res.sendStatus(500);
+    console.log(e)
+  }
+});
+
+controller.get('/:id', async (req: Request, res: Response) => {
+    const {id} = req.params
+    try {
+      const result = await Album.findById(id).populate('artist','title description image');
+      if (result) {
+        res.send(result)
+      } else {
+        res.sendStatus(404)
+      }
+    } catch (error) {
+      res.sendStatus(500)
+    }
+  })
+
+controller.post("/", upload.single("image"), async (req: Request, res: Response) => {
+    const { title, artist, release_date} = req.body as CreateAlbumDto;
+    let image = "";
+    if (req.file) {
+      image = req.file.filename;
+    }
+    const newArtist = new CreateAlbumDto(title, artist, release_date, image);
+    const result = new Album(newArtist);
+    await result.save();
+    res.send(result);
+    try {
+    } catch (e) {
+      res.status(400).send(e);
+    }
+  }
+);
+
+export default controller;
--- music-api/src/server.ts
+++ music-api/src/server.ts
@@ -3,7 +3,8 @@ import logger from "jet-logger/lib";
 import cors from "cors";
 import mongoose from "mongoose";
 import process from "process";
-import artistsController from '@src/controllers/artists.controller'
+import artistsController from '@src/controllers/artists.controller';
+import albumController from '@src/controllers/albums.controller';
 
 const app = express();
 const PORT = 8000;
@@ -26,6 +27,7 @@ app.use(express.static("public"));
 app.use(cors());
 
 // Controllers
-app.use('/artists', artistsController)
+app.use('/artists', artistsController);
+app.use('/albums', albumController);
 
 run().catch((e) => logger.err(e)); 
